6c2afa23c8b1e1ba28aa835c1caf6a4977635a71
Ivan Arkhipov <vanchez666@gmail.com>
2024-03-14 16:39:13 +0300

    5-1 task is ready

--- 05-dom-document-loading/1-notification/index.js
+++ 05-dom-document-loading/1-notification/index.js
@@ -1,3 +1,60 @@
 export default class NotificationMessage {
+  static lastInstance;
 
+  constructor(
+    message = '',
+    {duration = 0, type = ''} = {}
+  ) {
+    
+    this.message = message;
+    this.duration = duration;
+    this.type = type;
+    
+    this.timerId = null;
+
+    this.element = this.createElement(this.createTemplate());
+  }
+  
+  show(container = document.body) {
+    if (NotificationMessage.lastInstance) {
+      NotificationMessage.lastInstance.destroy();
+    }
+    NotificationMessage.lastInstance = this;
+    container.append(this.element);
+    this.timerId = setTimeout(() => this.destroy(), this.duration);    
+  }
+
+  createElement(template) {
+    const element = document.createElement('div');
+
+    element.innerHTML = template;
+
+    return element.firstElementChild;
+  }
+
+  createTemplate() {
+    return (`
+      <div class="notification ${this.type}" style="--value:${(this.duration / 1000).toFixed(3)}s">
+        <div class="timer"></div>
+        <div class="inner-wrapper">
+          <div class="notification-header">${this.type}</div>
+          <div class="notification-body">
+            ${this.message}
+          </div>
+        </div>
+      </div>
+    `);
+  }
+
+  remove() {
+    this.element.remove();
+    NotificationMessage.lastInstance = null;
+  }
+  
+  destroy() {
+    this.remove();
+    if (this.timerId) {
+      clearTimeout(this.timerId);
+    }
+  }
 }
