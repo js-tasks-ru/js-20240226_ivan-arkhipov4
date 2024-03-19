f557c491389595af6d4e2c9e48513d6d0472197b
Ivan Arkhipov <vanchez666@gmail.com>
2024-03-19 21:48:27 +0300

    6-1 'Sortable-table-v2' ready

--- 06-events-practice/1-sortable-table-v2/index.js
+++ 06-events-practice/1-sortable-table-v2/index.js
@@ -1,8 +1,48 @@
-export default class SortableTable {
+import SortableTableV1 from '../../05-dom-document-loading/2-sortable-table-v1/index.js';
+
+export default class SortableTableV2 extends SortableTableV1 {
+  defaultSortField = 'title';
+  defaultSortOrder = 'asc';
+
   constructor(headersConfig, {
     data = [],
     sorted = {}
   } = {}) {
+    super(headersConfig, data);
+    this.sorted = sorted;
 
+    this.createEvenListeners();
+
+    this.sort(this.defaultSortField, this.defaultSortOrder);
+  }
+
+  createEvenListeners() {
+    this.subElements.header.addEventListener(
+      'pointerdown', (e) => this.onHeaderPointerDown(e)
+    );
+  }
+
+  onHeaderPointerDown(e) {
+    const columnElement = e.target.closest('[data-sortable=true]');
+
+    if (!columnElement) {
+      return;
+    }
+
+    const orderValue = columnElement.dataset.order === 'desc' ? 'asc' : 'desc';
+    const columnId = columnElement.dataset.id;
+
+    this.sort(columnId, orderValue);
+  }
+
+  destroyEventListeners() { 
+    this.subElements.header.removeEventListener(
+      'pointerdown', (e) => this.onHeaderPointerDown(e)
+    );
+  }
+
+  destroy() {
+    super.destroy();
+    this.destroyEventListeners();
   }
 }
